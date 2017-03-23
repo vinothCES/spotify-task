354918130c4bebb96fc58bf8b1f8317549caef63
Vinoth Jayaprakash <vinothj@CESCHN.COM>
2017-03-23 18:15:54 +0530

    Adding gulp file with common tasks

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+
+var jsSources = ['app/*.js', 'app/**/*.js'],
+  htmlSources = ['**/*.html', 'app/**/*.html'];
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['html', 'js', 'watch', 'connect']);
